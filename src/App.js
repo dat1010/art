ba6a5db2bd804e9e2c0cab6cf34b2b2e20773d3c
David <tannerdjr@gmail.com>
2021-04-26 22:35:45 -0400

    last images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Carousel from 'react-gallery-carousel';
 import 'react-gallery-carousel/dist/index.css';
 
 const App = () => {
-  const images = ["babyYoda.jpg", "fett.jpg", "lion.jpg"].map((size) => ({
+  const images = ["babyYoda.jpg", "fett.jpg", "lion.jpg", "w3.jpg","wantToKnowHowIGotTheseScars.jpg"].map((size) => ({
     src: `${size}`
   }));
 
